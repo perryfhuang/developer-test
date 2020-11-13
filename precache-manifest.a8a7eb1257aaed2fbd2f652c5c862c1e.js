142bd651161c583fb17abc5e9ba3bf59e445938f
perryfhuang <perryfhuang@gmail.com>
2020-11-13 10:59:16 -0500

    Updates

---
+++ precache-manifest.a8a7eb1257aaed2fbd2f652c5c862c1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70f113ce55549c510df5a3f7263b08a9",
+    "url": "/developer-test/index.html"
+  },
+  {
+    "revision": "0b05ac285e4611fa4136",
+    "url": "/developer-test/static/css/main.d094bdd2.chunk.css"
+  },
+  {
+    "revision": "5e6e3578b69bb2f80cf5",
+    "url": "/developer-test/static/js/2.1a57711a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/developer-test/static/js/2.1a57711a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b05ac285e4611fa4136",
+    "url": "/developer-test/static/js/main.1a4df7bf.chunk.js"
+  },
+  {
+    "revision": "082bbd23f2869e01e0ad",
+    "url": "/developer-test/static/js/runtime-main.741df866.js"
+  }
+]);
